d67b5f864cbefa703cd8f2d32189183771728efa
Charlie <765914173@qq.com>
2018-04-14 15:22:12 +0800

    HomePage-Top

---
+++ src/views/home/home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="all">
+
+      <!-- Top Navigation -->
+      <div class="top">
+          <!-- Top Navigation left:img+Welcome --> 
+          <div class="top_left">
+              <div class="top_left_img">
+                  <img src="../../assets/logo.png">
+              </div>
+               <h3>welcome to Charlie Factory</h3>
+          </div>
+          <!-- Top Navigation ringht:shoppingCart+Login+Select -->
+          <div class="top_right">
+              <div class="shoppingCart">
+                  <p>Shopping Cart</p>
+              </div>
+              <div class="Login">
+                  <p>Login</p>
+              </div>
+              <div class="select">
+                  <input class="input" type="text" placeholder="Select what you want">
+                  <button>Done</button>  
+              </div>
+          </div>
+      </div>
+  </div>
+  
+</template>
+
+<style>
+.top{
+    width: 100%;
+    height: 40px;
+}
+</style>
+
